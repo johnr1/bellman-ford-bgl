f0bea03bd9b0d4be9d25b987fc86a61101f47fd4
John Rizos <giannisrizos1@gmail.com>
2018-05-19 01:30:35 +0300

    Added argument parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,91 @@
 #include <iostream>
 #include <ctime>
 #include <LEDA/graph/graph.h>
+#include <boost/graph/random.hpp>
+#include <boost/random/mersenne_twister.hpp>
 
 #include "../include/graph.h"
 #include "../include/graph_gen.h"
 #include "../include/benchmark.h"
 #include "../include/run.h"
 
+enum class Command {benchmark, run};
+enum class GraphType {random, grid};
+
+Command command;
+unsigned N=200;
+GraphType graph_type = GraphType::random;
+
+void printUsage(std::string programName) {
+    std::cout << "Usage:  "<< programName << " benchmark\n"
+              << "or:\t" << programName << " run [-t type] [-n vertexes]\n\n"
+              << "benchmark\tAccurately times all algorithms for all test graphs and prints results.\n"
+              << "run\t\tPerforms a run of the bellman ford algorithm, tests for integrity, prints results.\n"
+              << "-t\t\tThe type of the graph ['random' | 'grid'] (default random) \n"
+              << "-n\t\tNumber of vertexes for random graph(default 200)\n"
+              << "\t\tNumber of vertexes in one dimension of grid (Total: n*n) (default 200)\n"
+              << "--help\t\tPrint this message.\n";
+}
+
+void parseArguments(int argc, char *argv[]) {
+    command = Command::run;
+    if(argc < 2){
+        std::cout << "No argument given. Running default action. Pass '--help' for more actions." << std::endl;
+        return;
+    }
+
+    int i = 1;
+    while(i < argc) {
+        std::string arg(argv[i]);
+
+        if(arg == "benchmark") {
+            command = Command::benchmark;
+            return;
+        }
+
+        else if(arg == "run") {
+            command = Command::run;
+        }
+        else if(arg == "-n" && i+1 < argc) {
+            int tmp = atoi(argv[++i]);
+            N = tmp != 0 ? tmp : 300;
+        }
+        else if(arg == "-t" && i+1 < argc){
+            std::string gt(argv[++i]);
+            if(gt == "random") graph_type = GraphType::random;
+            else if(gt == "grid")graph_type = GraphType::grid;
+        }
+
+        else{
+            printUsage(argv[0]);
+            exit(2);
+        }
+        ++i;
+    }
+}
+
 int main(int argc, char* argv[]) {
-    srand (static_cast<unsigned>(time(NULL))); // seed RNG
+    parseArguments(argc, argv);
+    srand (time(NULL)); // seed RNG
+    boost::mt19937 gen(time(NULL));
 
-    Graph BG = randomGraph(50);
-    auto LG = boostToLeda(BG);
-    auto Ls = LG.choose_node();
-    auto Bs = LG[Ls];
+    if(command == Command::benchmark){
+        benchmark();
+    }
+    else if(command == Command::run){
+        Graph G;
+        Vertex s;
+        if(graph_type == GraphType::random) {
+            G = randomGraph(N);
+            s = boost::random_vertex(G, gen);
+        }
+        else {
+            G = myGridGraph(N);
+            s = 0;
+        }
 
-    run_my_bf(BG, Bs);
-    run_boost_bf(BG,Bs);
-    run_leda_bf(LG, Ls);
+        run_boost_bf(G, s);
+    }
 
-    //benchmark();
     return 0;
 }
